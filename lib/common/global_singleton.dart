7b6bf19473bdd87e23acb24b560bc4cdfa1c3d9e
KennethHung <maple135790@gmail.com>
2023-05-27 11:55:41 +0800

    chore: 更新離線測試資料

--- lib/common/global_singleton.dart
+++ lib/common/global_singleton.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:x50pay/common/models/user/user.dart';
 import 'package:x50pay/repository/repository.dart';
@@ -36,22 +38,17 @@ class GlobalSingleton {
         return true;
       } else {
         if (_costEnabled) _point -= 20;
-        user = UserModel(
-            message: "done",
-            code: 200,
-            userimg: 'https://secure.gravatar.com/avatar/6a4cbe004cdedee9738d82fe9670b326?size=250',
-            email: "maple135790@gmail.com",
-            uid: '938',
-            point: _point,
-            name: "testUser (Offline)",
-            ticketint: 9,
-            phoneactive: true,
-            vip: true,
-            vipdate: const VipDate(date: 1657660411780),
-            sid: "",
-            sixn: "575707",
-            tphone: 1,
-            doorpwd: "本期門禁密碼爲 : 1743#");
+        final customMap = {
+          "point": _point,
+          "email": "testUser@testUser",
+          "name": "testUser",
+        };
+        var rawUserJson = jsonDecode(
+                r"""{"message":"done","code":200,"userimg":"https://secure.gravatar.com/avatar/6a4cbe004cdedee9738d82fe9670b326?size=250","uid":"938","ticketint":2,"phoneactive":true,"vip":true,"vipdate":{"$date":1685957759681},"sid":"","sixn":"842232","tphone":1,"doorpwd":"\u672c\u671f\u9580\u7981\u5bc6\u78bc\u7232 : 1743#"}""")
+            as Map<String, dynamic>
+          ..addAll(customMap);
+        user = UserModel.fromJson(rawUserJson);
+
         return true;
       }
     } on Exception catch (_) {
